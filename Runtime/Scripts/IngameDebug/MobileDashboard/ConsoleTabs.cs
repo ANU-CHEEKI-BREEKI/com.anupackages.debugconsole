92ae0b3c00cef7aab3bd31e9b3661bb4c748e570
ANU <titan.x7@gmail.com>
2023-11-03 11:34:38 +0200

    saving last tab opened

--- Runtime/Scripts/IngameDebug/MobileDashboard/ConsoleTabs.cs
+++ Runtime/Scripts/IngameDebug/MobileDashboard/ConsoleTabs.cs
@@ -12,6 +12,20 @@ namespace ANU.IngameDebug.Console.Dashboard
         [SerializeField] private GameObject _consoleObject;
         [SerializeField] private GameObject _dashboardObject;
 
+        private int DefaultTab =>
+#if !UNITY_EDITOR &&(UNITY_ANDROID || UNITY_IOS)
+            1
+#else
+            0
+#endif
+            ;
+
+        private int LastTabIndex
+        {
+            get => PlayerPrefs.GetInt("ConsoleLastTabIndex", DefaultTab);
+            set => PlayerPrefs.SetInt("ConsoleLastTabIndex", value);
+        }
+
         private void Awake()
         {
             var group = GetComponent<ToggleGroup>();
@@ -20,10 +34,13 @@ namespace ANU.IngameDebug.Console.Dashboard
             _console.group = group;
             _dashboard.group = group;
 
-
             _console.isOn = false;
             _dashboard.isOn = false;
-            _console.isOn = true;
+
+            var targetTab = LastTabIndex == 0
+                ? _console
+                : _dashboard;
+            targetTab.isOn = true;
 
             _console.onValueChanged.AddListener(isOn => UpdateTabs());
             _dashboard.onValueChanged.AddListener(isOn => UpdateTabs());
@@ -35,6 +52,8 @@ namespace ANU.IngameDebug.Console.Dashboard
         {
             _consoleObject.SetActive(_console.isOn);
             _dashboardObject.SetActive(_dashboard.isOn);
+
+            LastTabIndex = _console.isOn ? 0 : 1;
         }
     }
 }
