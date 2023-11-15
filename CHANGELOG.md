12472a76130ba54b9779cce1bbbd4396f6e2198a
ANU <titan.x7@gmail.com>
2023-11-15 12:48:16 +0200

    bugfix of dropdown popup out of screen

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - console.set-theme command. removed unnecessary args. auto complete now able to handle argument values selection quickly
 - set scale command for dashboard specific
 - load scene. removed unnecessary args. separated to 2 commands for load and reload scene
+- dropdown out of screen bug fix
 
 ## [2.1.0] - 2023-11-13
 ### Added
--- Runtime/Scripts/IngameDebug/MobileDashboard/AdaptiveDropdownList.cs
+++ Runtime/Scripts/IngameDebug/MobileDashboard/AdaptiveDropdownList.cs
@@ -7,12 +7,45 @@ namespace ANU.IngameDebug.Console.Dashboard
     [RequireComponent(typeof(RectTransform))]
     public class AdaptiveDropdownList : MonoBehaviour
     {
-        private void OnEnable()
+        private bool _lateUpdate;
+
+        private void OnEnable() => _lateUpdate = true;
+
+        private void LateUpdate()
         {
+            if (!_lateUpdate)
+                return;
+            _lateUpdate = false;
+
             var text = GetComponentsInChildren<TMP_Text>();
             var max = text.Max(t => t.preferredWidth);
             var rt = transform as RectTransform;
-            rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, max);
+
+            // get root rect
+            var canvas = GetComponentInParent<Canvas>();
+            var rootRect = canvas.rootCanvas.transform as RectTransform;
+
+            // prepare for clamp position and size
+            var parent = transform.parent;
+            rt.SetParent(rootRect);
+            rt.anchorMin = Vector2.one / 2f;
+            rt.anchorMax = Vector2.one / 2f;
+
+            // get max size and clamp size
+            var size = rt.rect.size;
+            size.x = max;
+            var clampedSize = Vector2.Min(size, rootRect.rect.size);
+            rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, clampedSize.x);
+            rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, clampedSize.y);
+
+            // clamp position
+            var allowedPositionExtends = (rootRect.rect.size - clampedSize) / 2f;
+            var localPosition = rt.anchoredPosition;
+            localPosition.x = Mathf.Clamp(localPosition.x, rootRect.rect.center.x - allowedPositionExtends.x, rootRect.rect.center.x + allowedPositionExtends.x);
+            localPosition.y = Mathf.Clamp(localPosition.y, rootRect.rect.center.y - allowedPositionExtends.y, rootRect.rect.center.y + allowedPositionExtends.y);
+            rt.anchoredPosition = localPosition;
+
+            rt.SetParent(parent);
         }
     }
 }
